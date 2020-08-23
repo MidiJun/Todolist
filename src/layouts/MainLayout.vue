c02b81685c9e04ece24bf9912d3c67fce3dca849
GuoJun <junguo0908@gmail.com>
2020-08-23 11:43:32 +0800

    1.0--the original version learned from youtube

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,109 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+      </q-toolbar>
+
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="text-subtitle1">{{ dateNow }}</div>
+      </div>
+      <q-img src="../statics/bloom.jpg" class="header-image absolute-top" />
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      :width="300"
+      :breakpoint="400"
+    >
+      <q-scroll-area
+        style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd"
+      >
+        <q-list padding>
+          <q-item to="/" clickable v-ripple exact>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+
+            <q-item-section>
+              日常
+            </q-item-section>
+          </q-item>
+          <q-item to="/" clickable v-ripple exact>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+
+            <q-item-section>
+              工作
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <div class="absolute-top bg-teal-4" style="height: 192px">
+        <div
+          class="bg-transparent  fit row  justify-center items-center q-px-md"
+        >
+          <q-avatar size="56px" class="col-8    " align="center">
+            <img
+              src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595910886276&di=92785bb39c382fa604588c8f999aac61&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F20181%2F30%2F201813015155_Bfx8k.thumb.700_0.jpeg"
+            />
+          </q-avatar>
+          <div class="text-weight-bold col-4 text-h5" align="left">
+            霜序八
+          </div>
+        </div>
+      </div>
+    </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+// import EssentialLink from "components/EssentialLink.vue";
+import { date } from "quasar";
+
+export default {
+  name: "MainLayout",
+
+  components: {
+    // EssentialLink
+  },
+
+  data() {
+    return {
+      leftDrawerOpen: false
+    };
+  },
+  computed: {
+    dateNow: function() {
+      let timeStamp = Date.now();
+      return date.formatDate(timeStamp, "dddd D MMMM");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 90%;
+  // filter: grayscale(20%);
+}
+</style>
---
+++ src/pages/Todo.vue
@@ -0,0 +1,110 @@
+<template>
+  <q-page class="bg-grey-2 column">
+    <div class="row bg-primary q-pa-sm">
+      <q-input
+        v-model="newTask"
+        filled
+        placeholder="想要做点什么…"
+        dense
+        square
+        class="bg-white col"
+        @keyup.enter="addTask"
+      >
+        <template>
+          <q-btn round dense flat icon="add" @click="addTask" />
+        </template>
+      </q-input>
+    </div>
+
+    <q-list class="bg-white">
+      <q-item
+        v-for="(task, i) in tasks"
+        :key="i"
+        v-ripple
+        clickable
+        @click="task.done = !task.done"
+        :class="{ 'done bg-teal-1': task.done }"
+      >
+        <q-item-section avatar>
+          <q-checkbox
+            v-model="task.done"
+            color="primary"
+            class="no-pointer-events"
+          />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section side v-if="task.done" @click.stop="deleteTask(i)">
+          <q-btn flat round color="primary" dense icon="delete" />
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <div class="no-tasks absolute-center" v-if="!tasks.length">
+      <q-icon name="check" size="100px" color="primary" />
+      <div class="text-h5 text-primary text-center">No tasks</div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+  data() {
+    return {
+      newTask: "",
+      tasks: [
+        {
+          title: "打扫卫生",
+          done: false
+        },
+        {
+          title: "整理文档",
+          done: true
+        }
+        // {
+        //   title: "knkn能量补充",
+        //   done: false
+        // }
+      ]
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.$q
+        .dialog({
+          title: "提示",
+          message: "你要删除此任务吗?",
+          cancel: true,
+          persistent: true
+        })
+        .onOk(() => {
+          this.tasks.splice(index, 1);
+          this.$q.notify({
+            message: "此任务已删除",
+            icon: "done"
+          });
+        });
+    },
+    addTask() {
+      this.tasks.push({
+        title: this.newTask,
+        done: false
+      });
+      this.newTask = "";
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+.no-tasks {
+  opacity: 70%;
+}
+</style>
